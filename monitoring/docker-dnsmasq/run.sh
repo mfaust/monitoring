158fc2129c866017ecdd824f493477382125e781
Bodo Schulz <bodo.schulz@coremedia.com>
2016-03-04 15:04:52 +0100

    add docker container for DNS Querys between docker container
    temporary workaround before using etcd oder consul service

---
+++ monitoring/docker-dnsmasq/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+. config.rc
+
+if [ $(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $NF}' | wc -l) -gt 0 ]
+then
+  docker kill ${CONTAINER_NAME} 2> /dev/null
+  docker rm   ${CONTAINER_NAME} 2> /dev/null
+fi
+
+DOCKER_ADDN_DIR=${DOCKER_ADDN_DIR:-/tmp}
+
+# ---------------------------------------------------------------------------------------
+
+docker run \
+  --interactive \
+  --tty \
+  --detach \
+  --publish=5353:53 \
+  --volume=${DOCKER_ADDN_DIR}:/app \
+  --hostname=${USER}-${TYPE} \
+  --name ${CONTAINER_NAME} \
+  ${TAG_NAME}
+
+# ---------------------------------------------------------------------------------------
+# EOF
