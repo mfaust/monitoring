feb87cb9b6024cf3e659b85e1d6665a196ae45ac
Bodo Schulz <bodo.schulz@coremedia.com>
2016-05-26 17:45:56 +0200

    change run.sh

--- monitoring/docker-icinga2/run.sh
+++ monitoring/docker-icinga2/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-. config.rc
+# set -e
+# set -x
+
+SCRIPT=$(readlink -f ${0})
+BASE=$(dirname "${SCRIPT}")
+
+. ${BASE}/config.rc
 
 if [ $(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $NF}' | wc -l) -gt 0 ]
 then
@@ -22,27 +28,56 @@ DOCKER_DASHING_API_PASS=${DOCKER_DASHING_API_PASS:-icinga2ondashingr0xx}
 
 # ---------------------------------------------------------------------------------------
 
+docker_opts=
+docker_opts="${docker_opts} --interactive"
+docker_opts="${docker_opts} --tty"
+docker_opts="${docker_opts} --detach"
+docker_opts="${docker_opts} --hostname=${USER}-${TYPE}"
+docker_opts="${docker_opts} --name=${CONTAINER_NAME}"
+docker_opts="${docker_opts} --publish=5665:5665"
+docker_opts="${docker_opts} --publish=6666:6666"
+docker_opts="${docker_opts} --link=${USER}-mysql:database"
+docker_opts="${docker_opts} --link=${USER}-graphite:graphite"
+docker_opts="${docker_opts} --volume=${DOCKER_DATA_DIR}/monitoring:/var/cache/monitoring"
+docker_opts="${docker_opts} --volume=${BASE}/share/icinga2:/usr/local/monitoring"
+docker_opts="${docker_opts} --env MYSQL_HOST=${DATABASE_IP}"
+docker_opts="${docker_opts} --env MYSQL_PORT=3306"
+docker_opts="${docker_opts} --env MYSQL_USER=root"
+docker_opts="${docker_opts} --env MYSQL_PASS=${DOCKER_DBA_ROOT_PASS}"
+docker_opts="${docker_opts} --env IDO_PASSWORD=${DOCKER_IDO_PASS}"
+docker_opts="${docker_opts} --env CARBON_HOST=${GRAPHITE_IP}"
+docker_opts="${docker_opts} --env CARBON_PORT=2003"
+docker_opts="${docker_opts} --env DASHING_API_USER=${DOCKER_DASHING_API_USER}"
+docker_opts="${docker_opts} --env DASHING_API_PASS=${DOCKER_DASHING_API_PASS}"
+
+# ---------------------------------------------------------------------------------------
+
 docker run \
-  --interactive \
-  --tty \
-  --detach \
-  --publish=5665:5665 \
-  --publish=6666:6666 \
-  --volume=${PWD}/share/icinga2:/usr/local/monitoring \
-  --link=${USER}-mysql:database \
-  --link=${USER}-graphite:graphite \
-  --env MYSQL_HOST=${DATABASE_IP} \
-  --env MYSQL_PORT=3306 \
-  --env MYSQL_USER=root \
-  --env MYSQL_PASS=${DOCKER_DBA_ROOT_PASS} \
-  --env IDO_PASSWORD=${DOCKER_IDO_PASS} \
-  --env CARBON_HOST=${GRAPHITE_IP} \
-  --env CARBON_PORT=2003 \
-  --env DASHING_API_USER=${DOCKER_DASHING_API_USER} \
-  --env DASHING_API_PASS=${DOCKER_DASHING_API_PASS} \
-  --hostname=${USER}-${TYPE} \
-  --name ${CONTAINER_NAME} \
+  ${docker_opts} \
   ${TAG_NAME}
+
+# docker run \
+#   --interactive \
+#   --tty \
+#   --detach \
+#   --publish=5665:5665 \
+#   --publish=6666:6666 \
+#   --volume=${PWD}/share/icinga2:/usr/local/monitoring \
+#   --volume=${DOCKER_DATA_DIR}/monitoring:
+#   --link=${USER}-mysql:database \
+#   --link=${USER}-graphite:graphite \
+#   --env MYSQL_HOST=${DATABASE_IP} \
+#   --env MYSQL_PORT=3306 \
+#   --env MYSQL_USER=root \
+#   --env MYSQL_PASS=${DOCKER_DBA_ROOT_PASS} \
+#   --env IDO_PASSWORD=${DOCKER_IDO_PASS} \
+#   --env CARBON_HOST=${GRAPHITE_IP} \
+#   --env CARBON_PORT=2003 \
+#   --env DASHING_API_USER=${DOCKER_DASHING_API_USER} \
+#   --env DASHING_API_PASS=${DOCKER_DASHING_API_PASS} \
+#   --hostname=${USER}-${TYPE} \
+#   --name ${CONTAINER_NAME} \
+#   ${TAG_NAME}
 
 # ---------------------------------------------------------------------------------------
 # EOF
