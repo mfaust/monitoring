a5ed6c3586be3efd45c2cadf94a217e858d763dc
Bodo Schulz <bodo.schulz@coremedia.com>
2016-05-26 17:50:22 +0200

    clean up
    create DOCKER_DATA_DIR

--- monitoring/run-monitoring.sh
+++ monitoring/run-monitoring.sh
@@ -5,37 +5,19 @@
 SRC_BASE=${PWD}
 
 MONITORING_CONTAINER="docker-mysql docker-jolokia docker-graphite docker-icinga2 docker-icingaweb2 docker-grafana docker-dashing"
+MONITOR_DOCKER_CONTAINER=false
 
-# DOCKER_DNS="172.17.0.1"
-# DOCKER_ADDN_DIR="/tmp"
+export DOCKER_DNS="10.1.2.63"
+export DATA_DIR="/srv/docker/data"
 
-DOCKER_DNS="10.1.2.63"
-
-export DOCKER_DNS
-
-rm -f ${DOCKER_ADDN_DIR}/dnsmasq.addn.docker
-
-runDNSDocker() {
-
-  cd ${SRC_BASE}/docker-dnsmasq
-
-  . ./config.rc
-
-  echo " => starting DNS Container '${CONTAINER_NAME}' ..."
-
-  ./run.sh > /dev/null
-
-  IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CONTAINER_NAME})
-  NAME=$(docker inspect --format '{{ .Config.Hostname }}' ${CONTAINER_NAME})
-
-  echo "starting Docker DNS with IP ${IP}"
-
-  export DOCKER_DNS=${IP}
-  export DOCKER_ADDN_DIR="/tmp"
-}
-
+if [ ! -d ${DATA_DIR} ]
+then
+  mkdir -p ${DATA_DIR}
+fi
 
 runContainer() {
+
+  echo -e "\n"
 
   for d in ${MONITORING_CONTAINER}
   do
@@ -48,6 +30,17 @@ runContainer() {
       echo " => starting Container '${d}' named ${CONTAINER_NAME} ..."
 
       ./run.sh > /dev/null
+
+      if [ ${MONITOR_DOCKER_CONTAINER} == true ]
+      then
+        sleep 4s
+
+        IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CONTAINER_NAME})
+        NAME=$(docker inspect --format '{{ .Config.Hostname }}' ${CONTAINER_NAME})
+
+        ICINGA_API="$(readlink -f ../tools/bin/icinga-api.sh)"
+        ${ICINGA_API} --filter "docker-nod*" --address "${IP}" --name "${NAME}"
+      fi
     else
       echo "no run.sh found"
     fi
@@ -55,68 +48,13 @@ runContainer() {
     echo -e "\n =========================================================== \n"
   done
 
-  sleep 15s
+#  sleep 5s
+
+  echo -e " ... done\n\n"
 }
 
 runContainer
 
-cd ${SRC_BASE}
-
-for CID in $(docker ps -q)
-do
-  IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID})
-  NAME=$(docker inspect --format '{{ .Config.Hostname }}' ${CID})
-
-  echo " - '${IP}' - '${NAME}'"
-
-  echo "${IP}  ${NAME}" >> ${DOCKER_ADDN_DIR}/dnsmasq.addn.docker
-
-  tools/bin/icinga-api.sh --filter "docker-nod*" --address "${IP}" --name "${NAME}"
-done
-
 exit 0
-
-
-
-
-export DOCKER_DNS=${IP}
-# export DOCKER_ADDN_DIR="/tmp"
-
-for d in ${MONITORING_CONTAINER}
-do
-  if [ -x ${SRC_BASE}/${d}/run.sh ]
-  then
-    cd ${SRC_BASE}/${d}
-
-    . ./config.rc
-
-    echo " => starting Container '${d}' named ${CONTAINER_NAME} ..."
-
-    ./run.sh > /dev/null
-  else
-    echo "no run.sh found"
-  fi
-
-  echo -e "\n =========================================================== \n"
-done
-
-for CID in docker-dnsmasq ${MONITORING_CONTAINER} 
-do
-  IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID})
-  NAME=$(docker inspect --format '{{ .Config.Hostname }}' ${CID})
-  echo "${IP}  ${NAME}.${CONTAINER_DOMAIN}" >> ${DOCKER_ADDN_DIR}/dnsmasq.addn.docker
-done
-
-cd ${SRC_BASE}
-
-for CID in $(docker ps -q)
-do
-  IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID})
-  NAME=$(docker inspect --format '{{ .Config.Hostname }}' ${CID})
-
-  tools/bin/icinga-api.sh --filter "docker-nod*" --address "${IP}" --name "${NAME}.${CONTAINER_DOMAIN}"
-done
-
-#[ -x /usr/local/bin/update-docker-dns.sh ] && sudo /usr/local/bin/update-docker-dns.sh
 
 # EOF
