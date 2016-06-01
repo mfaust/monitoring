73b37a229c15e0c8a76568123d8c3cbbd91e8efc
Bodo Schulz <bodo.schulz@coremedia.com>
2016-06-01 10:58:22 +0200

    add fast and simple Docker Container for an OpenSources Nexus Version 2.x

---
+++ docker-nexus/README.md
@@ -0,0 +1,32 @@
+# docker-nexus
+
+Minimal Image with OSS Nexus 2.xx
+
+Based on alpine:edge
+
+## Build
+
+ ```
+ docker build --tag=docker-nexus .
+ ```
+ or
+
+ ```
+ ./build.sh
+ ```
+
+## run
+
+ ```
+ docker run docker-nexus
+ ```
+ or
+
+ ```
+ ./run.sh
+ ```
+
+## Ports
+
+* 8081
+
---
+++ docker-nexus/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+. config.rc
+
+if [ $(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $NF}' | wc -l) -gt 0 ]
+then
+  docker kill ${CONTAINER_NAME} 2> /dev/null
+  docker rm   ${CONTAINER_NAME} 2> /dev/null
+fi
+
+docker_opts=
+docker_opts="${docker_opts} --interactive"
+docker_opts="${docker_opts} --tty"
+docker_opts="${docker_opts} --detach"
+docker_opts="${docker_opts} --hostname ${USER}-${TYPE}"
+docker_opts="${docker_opts} --name ${CONTAINER_NAME}"
+docker_opts="${docker_opts} --volume /etc/localtime:/etc/localtime:ro"
+docker_opts="${docker_opts} --publish=8081:8081"
+
+if [ ! -z ${DOCKER_DNS} ]
+then
+  docker_opts="${docker_opts} --dns=${DOCKER_DNS}"
+fi
+
+# ---------------------------------------------------------------------------------------
+
+docker run \
+  ${docker_opts} \
+  ${TAG_NAME} /bin/sh
+
+# ---------------------------------------------------------------------------------------
+# EOF
