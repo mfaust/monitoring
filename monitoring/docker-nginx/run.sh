101ac6e6bbe3e1571a6e1a865c744697d971aee9
Bodo Schulz <bodo.schulz@coremedia.com>
2016-06-02 18:11:20 +0200

    add dingulary nginx container

---
+++ monitoring/docker-nginx/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+. config.rc
+
+if [ $(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $NF}' | wc -l) -gt 0 ]
+then
+  docker kill ${CONTAINER_NAME} 2> /dev/null
+  docker rm   ${CONTAINER_NAME} 2> /dev/null
+fi
+
+# ---------------------------------------------------------------------------------------
+
+docker_opts="${docker_opts} --interactive"
+docker_opts="${docker_opts} --tty"
+docker_opts="${docker_opts} --detach"
+docker_opts="${docker_opts} --publish 81:80"
+docker_opts="${docker_opts} --hostname ${USER}-${TYPE}"
+docker_opts="${docker_opts} --name ${CONTAINER_NAME}"
+docker_opts="${docker_opts} --volume /etc/localtime:/etc/localtime:ro"
+docker_opts="${docker_opts} --link ${USER}-icingaweb2:icingaweb2"
+docker_opts="${docker_opts} --link=${USER}-grafana:grafana"
+
+# ---------------------------------------------------------------------------------------
+
+docker run \
+  ${docker_opts} \
+  ${TAG_NAME}
+
+# ---------------------------------------------------------------------------------------
+# EOF
