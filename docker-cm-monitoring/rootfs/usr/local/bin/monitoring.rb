9e1fcacad785b622efab9ecd4196a9635457ac54
Bodo Schulz <bodo.schulz@coremedia.com>
2016-10-18 13:03:45 +0200

    switch to LogLevel INFO

--- docker-cm-monitoring/rootfs/usr/local/bin/monitoring.rb
+++ docker-cm-monitoring/rootfs/usr/local/bin/monitoring.rb
@@ -31,7 +31,7 @@ class Monitoring
     file.sync       = true
     @log            = Logger.new( file, 'weekly', 1024000 )
 #    @log = Logger.new( STDOUT )
-    @log.level      = Logger::DEBUG
+    @log.level      = Logger::INFO
     @log.datetime_format = "%Y-%m-%d %H:%M:%S::%3N"
     @log.formatter  = proc do |severity, datetime, progname, msg|
       "[#{datetime.strftime(@log.datetime_format)}] #{severity.ljust(5)} : #{msg}\n"
