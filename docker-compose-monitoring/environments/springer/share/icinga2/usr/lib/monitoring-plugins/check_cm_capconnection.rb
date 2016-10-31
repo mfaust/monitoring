d7d1cd02d985f60e2f16fe7c400328ce04dfe8ef
Bodo Schulz <bodo.schulz@coremedia.com>
2016-10-31 09:11:55 +0100

    add environments

---
+++ docker-compose-monitoring/environments/springer/share/icinga2/usr/lib/monitoring-plugins/check_cm_capconnection.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/ruby
+
+require_relative '/usr/local/lib/icingachecks.rb'
+
+# ---------------------------------------------------------------------------------------
+
+class Icinga2Check_CM_CapConnection < Icinga2Check
+
+  def initialize( settings = {} )
+
+    @log = logger()
+    @mc  = memcache()
+
+    MBean.logger( @log )
+
+    host         = settings[:host]        ? settings[:host]        : nil
+    application  = settings[:application] ? settings[:application] : nil
+    memory       = settings[:memory]      ? settings[:memory]      : nil
+
+    self.check( host, application, memory )
+
+  end
+
+
+  def check( host, application, type )
+
+    # get our bean
+    data = MBean.bean( host, application, 'CapConnection' )
+
+    if( data == false )
+      puts 'CRITICAL - Service not running!?'
+      exit STATE_CRITICAL
+    else
+
+      dataStatus    = data['status']    ? data['status']    : 500
+      dataTimestamp = data['timestamp'] ? data['timestamp'] : nil
+      dataValue     = ( data != nil && data['value'] ) ? data['value'] : nil
+
+      if( dataValue == nil )
+        puts 'CRITICAL - Service not running!?'
+        exit STATE_CRITICAL
+      end
+
+      beanTimeout,difference = beanTimeout?( dataTimestamp )
+
+      if( beanTimeout == STATE_CRITICAL )
+        puts sprintf( 'CRITICAL - last check creation is out of date (%d seconds)', difference )
+        exit beanTimeout
+      elsif( beanTimeout == STATE_WARNING )
+        puts sprintf( 'WARNING - last check creation is out of date (%d seconds)', difference )
+        exit beanTimeout
+      end
+
+      dataValue     = dataValue.values.first
+
+      state = dataValue['Open'] ? dataValue['Open'] : false
+
+      if( state == true )
+        status   = 'OK'
+        exitCode = STATE_OK
+      else
+        status   = 'CRITICAL'
+        exitCode = STATE_CRITICAL
+      end
+
+      puts sprintf( '%s - Cap Connection %s', status, state ? 'Open' : 'not existst' )
+      exit exitCode
+    end
+  end
+
+end
+
+# ---------------------------------------------------------------------------------------
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on('-h', '--host NAME'       , 'Host with running Application')                   { |v| options[:host]  = v }
+  opts.on('-a', '--application APP' , 'Name of the running Application')                 { |v| options[:application]  = v }
+
+end.parse!
+
+m = Icinga2Check_CM_CapConnection.new( options )
