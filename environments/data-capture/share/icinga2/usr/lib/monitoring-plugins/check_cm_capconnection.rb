810ace3c70f656cd9adf4b24a22449c6dad838aa
Bodo Schulz <bodo.schulz@coremedia.com>
2017-02-06 14:27:49 +0100

    take some file from All-In-One environment and put them into the data-capture Environment
    icinga2 enablement

---
+++ environments/data-capture/share/icinga2/usr/lib/monitoring-plugins/check_cm_capconnection.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/ruby
+
+require_relative '/usr/local/lib/icingachecks.rb'
+
+# ---------------------------------------------------------------------------------------
+
+class Icinga2Check_CM_CapConnection < Icinga2Check
+
+  def initialize( settings = {} )
+
+    super
+
+    host         = settings[:host]        ? settings[:host]        : nil
+    application  = settings[:application] ? settings[:application] : nil
+    memory       = settings[:memory]      ? settings[:memory]      : nil
+
+    host         = self.shortHostname( host )
+
+    self.check( host, application, memory )
+
+  end
+
+
+  def check( host, application, type )
+
+    # get our bean
+    data = @mbean.bean( host, application, 'CapConnection' )
+
+    dataValue = self.runningOrOutdated( data )
+
+    dataValue = dataValue.values.first
+
+    state = dataValue['Open'] ? dataValue['Open'] : false
+
+    if( state == true )
+      status   = 'OK'
+      exitCode = STATE_OK
+    else
+      status   = 'CRITICAL'
+      exitCode = STATE_CRITICAL
+    end
+
+    puts sprintf( '%s - Cap Connection %s', status, state ? 'Open' : 'not existst' )
+    exit exitCode
+
+  end
+
+end
+
+# ---------------------------------------------------------------------------------------
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on('-h', '--host NAME'       , 'Host with running Application')                   { |v| options[:host]  = v }
+  opts.on('-a', '--application APP' , 'Name of the running Application')                 { |v| options[:application]  = v }
+
+end.parse!
+
+m = Icinga2Check_CM_CapConnection.new( options )
