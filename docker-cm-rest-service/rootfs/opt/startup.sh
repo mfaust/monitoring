dc43b004b1ef9858f69f83262b2c3ce7794f58c2
Bodo Schulz <bodo.schulz@coremedia.com>
2017-03-25 15:26:21 +0100

    system care for docker-cm-rest-service

--- docker-cm-rest-service/rootfs/opt/startup.sh
+++ docker-cm-rest-service/rootfs/opt/startup.sh
@@ -1,46 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
-# set -x
-
-# GRAPHITE_HOST=${GRAPHITE_HOST:-graphite}
-# GRAPHITE_PORT=${GRAPHITE_PORT:-2003}
-
-# MEMCACHE_HOST=${MEMCACHE_HOST:-''}
-# MEMCACHE_PORT=${MEMCACHE_PORT:-11211}
 
 # -------------------------------------------------------------------------------------------------
-
-# trap ctrl-c and call ctrl_c()
-# trap ctrl_c SIGHUP SIGINT SIGTERM
-#
-# function ctrl_c() {
-#   echo "** Trapped CTRL-C"
-#
-#   exit 0
-# }
-
-# cfgFile="/etc/collectd/collectd.conf"
-#
-# createConfig() {
-#
-#   sed -i \
-#     -e "s/%GRAPHITE_HOST%/${GRAPHITE_HOST}/" \
-#     -e "s/%GRAPHITE_PORT%/${GRAPHITE_PORT}/" \
-#     ${cfgFile}
-# }
-#
-# startSupervisor() {
-#
-#   echo -e "\n Starting Supervisor.\n\n"
-#
-#   if [ -f /etc/supervisord.conf ]
-#   then
-#     /usr/bin/supervisord -c /etc/supervisord.conf >> /dev/null
-#   else
-#     exec /bin/sh
-#   fi
-# }
 
 addDNS() {
 
@@ -69,11 +31,7 @@ run() {
 
   addDNS
 
-#   createConfig
-
   /usr/local/bin/rest-service.rb
-
-#  startSupervisor
 }
 
 run
