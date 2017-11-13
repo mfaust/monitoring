a1c6b58d69db16ad7c916162021e41dfa2ec270d
Bodo Schulz <bodo.schulz@coremedia.com>
2017-11-13 09:24:02 +0100

    change some Dockerfiles to add healthchecks
    change integration of icina-cert-service
    add container for ssl-certificate building

--- docker-cm-dashing/rootfs/init/icinga_cert.sh
+++ docker-cm-dashing/rootfs/init/icinga_cert.sh
@@ -2,6 +2,13 @@
 ICINGA_API_PORT=${ICINGA_API_PORT:-5665}
 ICINGA_CERT_SERVICE=${ICINGA_CERT_SERVICE:-false}
 
+ICINGA_CERT_SERVICE_SERVER=
+ICINGA_CERT_SERVICE_PORT=
+ICINGA_CERT_SERVICE_BA_USER=
+ICINGA_CERT_SERVICE_BA_PASSWORD=
+ICINGA_CERT_SERVICE_API_USER=
+ICINGA_CERT_SERVICE_API_PASSWORD=
+
 # get a new icinga certificate from our icinga-master
 #
 #
@@ -149,6 +156,42 @@ validate_local_ca() {
   fi
 }
 
+
+extract_vars() {
+
+  if [ ! -z "${ICINGA_CERT_SERVICE}" ]
+  then
+    ICINGA_CERT_SERVICE_SERVER=$(echo "${ICINGA_CERT_SERVICE}" | jq --raw-output .server)
+    ICINGA_CERT_SERVICE_PORT=$(echo "${ICINGA_CERT_SERVICE}" | jq --raw-output .port)
+    ICINGA_CERT_SERVICE_PATH=$(echo "${ICINGA_CERT_SERVICE}" | jq --raw-output .path)
+    ICINGA_CERT_SERVICE_API_USER=$(echo "${ICINGA_CERT_SERVICE}" | jq --raw-output .api.user)
+    ICINGA_CERT_SERVICE_API_PASSWORD=$(echo "${ICINGA_CERT_SERVICE}" | jq --raw-output .api.password)
+    ICINGA_CERT_SERVICE_BA_USER=$(echo "${ICINGA_CERT_SERVICE}" | jq --raw-output .ba.user)
+    ICINGA_CERT_SERVICE_BA_PASSWORD=$(echo "${ICINGA_CERT_SERVICE}" | jq --raw-output .ba.password)
+
+    [ "${ICINGA_CERT_SERVICE_SERVER}" == null ] && ICINGA_CERT_SERVICE_SERVER=
+    [ "${ICINGA_CERT_SERVICE_PORT}" == null ] && ICINGA_CERT_SERVICE_PORT=4567
+    [ "${ICINGA_CERT_SERVICE_PATH}" == null ] && ICINGA_CERT_SERVICE_PATH="/"
+    [ "${ICINGA_CERT_SERVICE_API_USER}" == null ] && ICINGA_CERT_SERVICE_API_USER=
+    [ "${ICINGA_CERT_SERVICE_API_PASSWORD}" == null ] && ICINGA_CERT_SERVICE_API_PASSWORD=
+    [ "${ICINGA_CERT_SERVICE_BA_USER}" == null ] && ICINGA_CERT_SERVICE_BA_USER=
+    [ "${ICINGA_CERT_SERVICE_BA_PASSWORD}" == null ] && ICINGA_CERT_SERVICE_BA_PASSWORD=
+
+    if (
+      [ ! -z ${ICINGA_CERT_SERVICE_SERVER} ] &&
+      [ ! -z ${ICINGA_CERT_SERVICE_PORT} ] &&
+      [ ! -z ${ICINGA_CERT_SERVICE_BA_USER} ] &&
+      [ ! -z ${ICINGA_CERT_SERVICE_BA_PASSWORD} ] &&
+      [ ! -z ${ICINGA_CERT_SERVICE_API_USER} ] &&
+      [ ! -z ${ICINGA_CERT_SERVICE_API_PASSWORD} ]
+    )
+    then
+      ICINGA_CERT_SERVICE=true
+    fi
+  fi
+}
+
+extract_vars
 . /init/wait_for/cert_service.sh
 get_certificate
 
