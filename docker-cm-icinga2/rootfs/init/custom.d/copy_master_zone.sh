e52d096355f309b9e4b36bed31f8e2704498c812
Bodo Schulz <bodo.schulz@coremedia.com>
2018-01-24 09:25:17 +0100

    test for custom matser zone integration

---
+++ docker-cm-icinga2/rootfs/init/custom.d/copy_master_zone.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. /init/output.sh
+
+log_info " ------------------------------------------------------ "
+log_info " create custom master zone"
+
+
+echo "${ICINGA_TYPE}"
+
+
+log_info " ------------------------------------------------------ "
