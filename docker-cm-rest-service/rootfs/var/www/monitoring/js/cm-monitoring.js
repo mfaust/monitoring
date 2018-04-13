552fde9f55a62ddcf4d3602b14f2f67d2387137b
Bodo Schulz <bodo@boone-schulz.de>
2018-04-13 06:48:45 +0200

    play with modal dialogs

--- docker-cm-rest-service/rootfs/var/www/monitoring/js/cm-monitoring.js
+++ docker-cm-rest-service/rootfs/var/www/monitoring/js/cm-monitoring.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
     trHTML += '<td>' + elem.dns.short + '</td>';
     trHTML += '<td>' + toDate(elem.status.created) + '</td>';
     trHTML += '<td>';
-    trHTML += ' <a href="#" class="uk-icon-link add-annotation" uk-icon="commenting" uk-tooltip="add annotation"></a>';
+    trHTML += ' <a href="#" class="uk-icon-link add-annotation-for-host" uk-icon="commenting" uk-tooltip="add annotation"></a>';
     trHTML += ' <a href="#" class="uk-icon-link delete-host uk-text-danger uk-padding-small" uk-icon="trash" uk-tooltip="delete host"></a>';
     trHTML += '</td>';
     trHTML += '</tr>';
@@ -80,23 +80,94 @@ $(function() {
 
 $(function() {
 
-  var $table = $('#monitoring-hosts');
+  $('#monitoring-hosts').on('click', '.add-annotation-for-host', function(e) {
+    e.preventDefault();
+    e.target.blur();
 
-  $table.on('click', '.add-annotation', function() {
-
+    var $dialog = $('#modal-overflow');
     var $t = $(this)
     var $b = $t.closest('tr');
     var $hostname = $b.data('hostname');
-    //var $m = $t.find( '.msg-wrap' );
-    //var $e = $t.find( '.msg-edit' );
 
-    console.debug($t);
-    console.debug($hostname);
+//     console.debug($t);
+//     console.debug($hostname);
+
+    var dialogHTML = '';
+
+    dialogHTML += '<div class="uk-modal-dialog">';
+    dialogHTML += '  <button class="uk-modal-close-default" type="button" uk-close></button>';
+    dialogHTML += '  <div class="uk-modal-header"><h2 class="uk-modal-title">Add Annotation for Host ' + $hostname + '</h2></div>';
+    dialogHTML += '  <div class="uk-modal-body" uk-overflow-auto>';
+
+    dialogHTML += '    <ul class="uk-tab" data-uk-tab="{connect:\'#my-id\'}">';
+    dialogHTML += '      <li><a href="">Tab 1</a></li>';
+    dialogHTML += '      <li><a href="">Tab 2</a></li>';
+    dialogHTML += '      <li><a href="">Tab 3</a></li>';
+    dialogHTML += '    </ul>';
+    dialogHTML += '    <ul id="my-id" class="uk-switcher uk-margin">';
+    dialogHTML += '      <li><a href="#" id="autoplayer" data-uk-switcher-item="next"></a>';
+    dialogHTML += '       This slide contains a hidden link, that selects the next slide when clicked. The click is simulated by jacascript to mimic autoplay.';
+    dialogHTML += '        </li>';
+    dialogHTML += '      <li>Content 2';
+    dialogHTML += '        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+    dialogHTML += '      </li>';
+    dialogHTML += '      <li>Content 3</li>';
+    dialogHTML += '    </ul>';
+
+    dialogHTML += '    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+    dialogHTML += '  </div>';
+    dialogHTML += '  <div class="uk-modal-footer uk-text-right">';
+    dialogHTML += '    <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>';
+    dialogHTML += '    <button id="add-annotation" class="uk-button uk-button-primary" type="button">Save</button>';
+    dialogHTML += '  </div>';
+    dialogHTML += '</div>';
+
+    $dialog.html(dialogHTML);
+
+    UIkit.modal($dialog).show();
+
+    $dialog.on('click', '#add-annotation', function(e) {
+
+      console.debug($hostname);
+    });
   });
 
-  $('.add-annotation').on('click', function(event) {
+  $('#monitoring-hosts').on('click', '.delete-host', function() {
 
-    UIkit.modal('#modal-overflow').show();
+    var $dialog = $('#modal-overflow');
+    var $t = $(this)
+    var $b = $t.closest('tr');
+    var $hostname = $b.data('hostname');
+
+//     console.debug($t);
+//     console.debug($hostname);
+
+    var dialogHTML = '';
+
+    dialogHTML += '<div class="uk-modal-dialog">';
+    dialogHTML += '  <button class="uk-modal-close-default" type="button" uk-close></button>';
+    dialogHTML += '  <div class="uk-modal-header"><h2 class="uk-modal-title">delete Host ' + $hostname + ' from monitoring</h2></div>';
+    dialogHTML += '  <div class="uk-modal-body" uk-overflow-auto>';
+    dialogHTML += '    <p>schould delete this host from monitoring?</p>';
+    dialogHTML += '  </div>';
+    dialogHTML += '  <div class="uk-modal-footer uk-text-right">';
+    dialogHTML += '    <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>';
+    dialogHTML += '    <button id="delete" class="uk-button uk-button-primary" type="button">Delete</button>';
+    dialogHTML += '  </div>';
+    dialogHTML += '</div>';
+
+    $dialog.html(dialogHTML);
+
+    UIkit.modal.confirm('<b>Delete</b> Host ' + $hostname + ' from monitoring', {
+      labels: {
+        cancel: 'Cancel',
+        ok: 'DELETE this host from monitoring'
+      }
+    }).then(function () {
+      console.log('Confirmed.')
+    }, function () {
+      console.log('Rejected.')
+    });
   });
 });
 
